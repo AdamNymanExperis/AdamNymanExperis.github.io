e3c27856b00dce647365391aa0af22ec0edc6282
AdamNymanExperis <122439687+AdamNymanExperis@users.noreply.github.com>
2023-01-12 16:57:21 +0100

    testing some fixes to variable problem
    testing some fixes

--- app.js
+++ app.js
@@ -12,48 +12,64 @@ const selectedLaptopDescElement = document.getElementById('selectedLaptopDesc')
 const selectedLaptopPriceElement = document.getElementById('selectedLaptopPrice')
 const selectedLaptopImageElement = document.getElementById('selectedLaptopImage')
 
+let laptops = []
+const bankAccount = [20,0]
+//let balance = 2
 
 const addLaptopsToList = (laptops) => {
-    laptops.forEach(x => addLaptopToList(x));
-    //selectedLaptopPriceElement.innerText = laptops[0].price;
+    laptops.forEach(x => addLaptopToList(x))
     changeLaptopInfo(laptops[0])
 }
 
 const addLaptopToList = (laptop) => {
-    const laptopElement = document.createElement('option');
-    laptopElement.value = laptop.id;
-    laptopElement.appendChild(document.createTextNode(laptop.title));
-    laptopsElement.appendChild(laptopElement);
-}
-
-const handleLaptopListChange = e => {
-    const selectedLaptop = laptops[e.target.selectedIndex];
-    changeLaptopInfo(selectedLaptop)
+    const laptopElement = document.createElement('option')
+    laptopElement.value = laptop.id
+    laptopElement.appendChild(document.createTextNode(laptop.title))
+    laptopsElement.appendChild(laptopElement)
 }
 
 const changeLaptopInfo = selectedLaptop => {
-    selectedLaptopNameElement.innerText = selectedLaptop.title;
+    selectedLaptopNameElement.innerText = selectedLaptop.title
     //const localImageUrl = await fetchImage(selectedLaptop.image)
     //selectedLaptopImageElement.src = localImageUrl
-    selectedLaptopDescElement.innerText = selectedLaptop.description;
-    selectedLaptopPriceElement.innerText = selectedLaptop.price;
+    selectedLaptopDescElement.innerText = selectedLaptop.description
+    selectedLaptopPriceElement.innerText = selectedLaptop.price
 }
 
-laptopsElement.addEventListener("change", handleLaptopListChange);
+const handleLaptopListChange = e => {
+    const selectedLaptop = laptops[e.target.selectedIndex]
+    changeLaptopInfo(selectedLaptop)
+}
 
-const checkBalanceOrLoan = balance => {
-    if (balance >= 0){
+const handleLoanBtnClick = e => {
+    const wantedLoan = prompt('please enter the amount of money you want to loan! (not more than twice your current bank balance)')
+    if (parseFloat(wantedLoan) <= (bankAccount[0]*2)){
+        bankAccount[0] += wantedLoan
+        console.log(bankAccount)
+        alert('The loan was successful')
+    } else {
+        alert('The wanted amount was to high compared to current balance')
+    } 
+}
+
+laptopsElement.addEventListener('change', handleLaptopListChange)
+loanBtnElement.addEventListener('click', handleLoanBtnClick)
+
+const checkBalanceOrLoan = bankAccount => {
+    balanceValueElement.innerText = bankAccount[0]
+    if (bankAccount[1] >= 0){
         balanceElement.style.display = 'inline'
         loanElement.style.display = 'none'
+        loanBtnElement.style.display = 'block'
     } else {
         balanceElement.style.display = 'none'
         loanElement.style.display = 'inline'
+        loanBtnElement.style.display = 'none'
     }
 }
 
-let laptops = []
+
 laptops = await fetchLaptops()
 addLaptopsToList(laptops)
-let balance = -50
 
-checkBalanceOrLoan(balance)
+checkBalanceOrLoan(bankAccount)
